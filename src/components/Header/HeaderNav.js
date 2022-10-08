7b456f8f56da1074269f8e219691d8b057f72254
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2022-10-08 10:38:53 -0600

    mobile menu

--- src/components/Header/HeaderNav.js
+++ src/components/Header/HeaderNav.js
@@ -1,38 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import HeaderList from "./HeaderList";
 
 const classOption = {
+	default: "header-menu",
 	active: "header-menu active",
 	inactive: "header-menu inactive"
 };
 
+function changeClassMenu(classStr) {
+	const classList = classStr.split(" ");
+	return classList[1] === "inactive"
+		? classOption.active
+		: classOption.inactive;
+}
+
 export default function HeaderNav() {
 	const [menu, setMenu] = useState("header-menu");
 
+	useEffect(() => {
+		const mql = window.matchMedia("(min-width: 768px)");
+		mql.addEventListener("change", e => {
+			if (e.matches) {
+				setMenu(changeClassMenu(classOption.default));
+			} else {
+				setMenu(changeClassMenu(classOption.inactive));
+			}
+		});
+
+		return () => {
+			mql.removeEventListener("change", () => {
+				console.log("removed");
+			});
+		};
+	}, []);
+
 	function mobileHandler() {
-		const classList = menu.split(" ");
-		const newClass =
-			classList[1] === "inactive" ? classOption.active : classOption.inactive;
+		const newClass = changeClassMenu(menu);
 
 		setMenu(newClass);
 	}
 
 	return (
 		<>
-		<div className="mobile-menu-btn">
-			<button
-				onClick={mobileHandler}
-				className="btn-menu"
-				aria-label="menu"
-				type="button"
-			>
-				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="21">
-					<g fill="#D0D6F9" fillRule="evenodd">
-						<path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z" />
-					</g>
-				</svg>
-			</button>
-		</div>
+			<div className="mobile-menu-btn">
+				<button
+					onClick={mobileHandler}
+					className="btn-menu"
+					aria-label="menu"
+					type="button"
+				>
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="21">
+						<g fill="#D0D6F9" fillRule="evenodd">
+							<path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z" />
+						</g>
+					</svg>
+				</button>
+			</div>
 			<div className={menu}>
 				<div className="header-menu-action" align="right">
 					<button
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <div className="App">
       <div className="hero">
-        <h1 className="hero-title nav-text">So, you want to travel to</h1>
-        <h2 className="heading-1 hero-title-2">Space</h2>
-        <p className="hero-description ">
+        <h1 className="hero-title heading-5">So, you want to travel to</h1>
+        <h2 className="hero-title-2 heading-1">Space</h2>
+        <p className="hero-description body-text">
           Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
           back, and relax because we’ll give you a truly out of this world
