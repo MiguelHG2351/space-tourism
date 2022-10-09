1774b278f9d9c5b12908cbd1b5492f377f6f886d
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2022-10-09 07:58:00 -0600

    responsive design 1199px

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,19 +3,21 @@ import "../css/home.css";
 
 export default function Home() {
   return (
-    <div className="App">
-      <div className="hero">
-        <h1 className="hero-title heading-5">So, you want to travel to</h1>
-        <h2 className="hero-title-2 heading-1">Space</h2>
-        <p className="hero-description body-text">
-          Let’s face it; if you want to go to space, you might as well genuinely
-          go to outer space and not hover kind of on the edge of it. Well sit
-          back, and relax because we’ll give you a truly out of this world
-          experience!
-        </p>
-      </div>
-      <div className="explore">
-        <button type="button" className="btn-explore heading-4">Explore</button>
+    <div className="home-container">
+      <div className="hero-container">
+        <div className="hero">
+          <h1 className="hero-title heading-5">So, you want to travel to</h1>
+          <h2 className="hero-title-2 heading-1">Space</h2>
+          <p className="hero-description body-text">
+            Let’s face it; if you want to go to space, you might as well genuinely
+            go to outer space and not hover kind of on the edge of it. Well sit
+            back, and relax because we’ll give you a truly out of this world
+            experience!
+          </p>
+        </div>
+        <div className="explore">
+          <button type="button" className="btn-explore heading-4">Explore</button>
+        </div>
       </div>
     </div>
   );
